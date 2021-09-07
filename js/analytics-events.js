c4356b7eb4b327af634486086ebe4bc400de9a0f
unknown <eric.bryant0@gmail.com>
2021-09-07 17:01:49 -0400

    analytics tracking

---
+++ js/analytics-events.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('body a')
+
+links.forEach((link) => {
+  let linkCheck = new RegExp('/' + window.location.host + '/')
+  if (!linkCheck.test(link.href)) {
+    const linkName = link.innerText || link.getAttribute('title')
+    gtag('event', 'Click', {
+      event_category: 'External Link',
+      event_label: link.href,
+      value: linkName,
+    })
+  }
+})
