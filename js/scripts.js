ee3bac8bfbe81c47c7ca87efdc25a8d8e1873d33
unknown <eric.bryant0@gmail.com>
2021-09-14 13:08:53 -0400

    light & dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,35 @@ const heroSection = document.querySelector('.hero-section')
 const menuToggle = document.querySelector('.menu-toggle')
 const mobileMenu = document.querySelector('.mobile-menu')
 const mobileMenuItems = Array.from(document.querySelectorAll('.mobile-menu a'))
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+const currentTheme = localStorage.getItem('theme')
+const themeToggle = document.querySelector('#theme-toggle')
+
+if (!currentTheme) {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle('dark-theme')
+    localStorage.setItem('theme', 'dark')
+  } else {
+    document.body.classList.toggle('light-theme')
+    localStorage.setItem('theme', 'light')
+  }
+} else if (currentTheme == 'dark') {
+  document.body.classList.toggle('dark-theme')
+} else {
+  document.body.classList.toggle('light-theme')
+}
+
+themeToggle.addEventListener('click', function () {
+  let theme
+  document.body.classList.toggle('dark-theme')
+  document.body.classList.toggle('light-theme')
+  if (currentTheme == 'dark') {
+    theme = 'light'
+  } else {
+    theme = 'dark'
+  }
+  localStorage.setItem('theme', theme)
+})
 
 heroSection.style.marginTop = navigationWrapper.offsetHeight + 'px'
 
